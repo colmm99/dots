36ed76db50ede911c0de8eb3851fcb711960cd8e
Colm Monks <colmomonks@gmail.com>
2016-04-18 10:43:14 +0100

    some minor changes

--- scripts/arch_apps.sh
+++ scripts/arch_apps.sh
@@ -2,10 +2,12 @@
 
 PACKAGE_LIST="vim 
               	i3 feh
-	      	rdesktop terminator
+	      	rdesktop 
+		terminator
 		cmatrix 
 		iotop htop glances 
 		zsh 
+		pm-utils
 		ansible"
 
 sudo pacman -Sy ${PACKAGE_LIST}
