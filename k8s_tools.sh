fc049a9c92f8335e5a50f460c4c6c4881c385597
Colm Monks <cmonks@axway.com>
2018-07-09 09:16:03 +0100

    Add k8s tools

---
+++ k8s_tools.sh
@@ -0,0 +1,7 @@
+# k8s Tools
+
+curl -LO https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens
+
+curl -LO https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx
+
+curl -LO https://raw.githubusercontent.com/jonmosco/kube-ps1/master/kube-ps1.sh
