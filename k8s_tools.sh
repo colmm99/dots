909c5ba891f7de9c01374f9a25a27b101da9a6a4
Colm Monks <colmomonks@gmail.com>
2018-08-25 23:27:56 +0100

    Update package list

--- k8s_tools.sh
+++ k8s_tools.sh
@@ -1,7 +1,14 @@
-# k8s Tools
+#!/bin/bash
 
+# k8s Tools
 curl -LO https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens
+chmod +x kubens
+sudo mv kubens /usr/local/bin/
 
 curl -LO https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx
+chmod +x kubectx
+sudo mv kubectx /usr/local/bin/
 
 curl -LO https://raw.githubusercontent.com/jonmosco/kube-ps1/master/kube-ps1.sh
+chmod +x kube-ps1.sh
+sudo mv kube-ps1.sh /usr/local/bin/
--- scripts/arch_apps.sh
+++ scripts/arch_apps.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
 
-sudo pacman-mirrors -c Ireland,United_Kingdom,France
+sudo pacman-mirrors -c United_Kingdom,France
 
 PACKAGE_LIST="feh
 	      	rdesktop 
                 git
-		TERMINAtor
+		terminator
 		cmatrix 
 		iotop htop glances 
 		zsh 
                 tmux
                 perf
-		pm-utils
                 python-pip
 		firefox openssh docker
                 terraform
+		snapd
 		ansible"
 
+		# Old Apps
+		#pm-utils
 sudo pacman -Sy ${PACKAGE_LIST}
 
 sudo pip install awscli
+
+
+echo "Enable Docker Service and reboot
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+Reboot so current user can run docker without sudo
+
+"
+
+sudo systemctl start snapd
+sudo systemctl enable snapd
+sudo ln -s /var/lib/snapd/snap /snap
+
+sudo snap install kubectl --classic
+sudo snap install chromium --classic
+
+
+echo "Add /snap/bin to your PATH"
+
+echo "Install OhMyZSH"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
