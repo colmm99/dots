78a818dde583d49ccef0bb79874c98a8e94ef424
Colm Monks <colmomonks@gmail.com>
2018-10-11 21:33:20 +0100

    use process from sample

--- k8s_tools.sh
+++ k8s_tools.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 # k8s Tools
-curl -LO https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens
-chmod +x kubens
-sudo mv kubens /usr/local/bin/
-
-curl -LO https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx
-chmod +x kubectx
-sudo mv kubectx /usr/local/bin/
-
-curl -LO https://raw.githubusercontent.com/jonmosco/kube-ps1/master/kube-ps1.sh
-chmod +x kube-ps1.sh
-sudo mv kube-ps1.sh /usr/local/bin/
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
