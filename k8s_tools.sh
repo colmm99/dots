69c73d9a36bbf97fbb6d3fe714ba74910fcc6531
Colm Monks <colmomonks@gmail.com>
2019-07-11 23:46:10 +0100

    Add kube-ps1.sh

--- k8s_tools.sh
+++ k8s_tools.sh
@@ -4,3 +4,9 @@
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+if [ ! -f ~/kube-ps1.sh ];then
+    cd ~
+    curl -LO https://raw.githubusercontent.com/jonmosco/kube-ps1/master/kube-ps1.sh
+    cd
+fi
