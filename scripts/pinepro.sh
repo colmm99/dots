8b3566a1aed62d13c806fc0c642910cbe32424cd
Colm <++colmomonks@gmail.com>
2020-01-19 23:00:34 +0000

    Updates for pinebook pro

--- scripts/pinepro.sh
+++ scripts/pinepro.sh
@@ -42,3 +42,12 @@ echo "Add /snap/bin to your PATH"
 
 echo "Install OhMyZSH"
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+
+echo "Pinepook Prod Update Repo"
+if [ ! -d ~/Projects/updates_repo ];then
+    cd ~/Projects
+    git clone https://github.com/mrfixit2001/updates_repo.git updates_repo
+    cd -
+fi
+
